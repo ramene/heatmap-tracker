960543a1cda7b66fccfd2bb6caf8894f1ec705db
Maksim Rubanau <mirubanov@gmail.com>
2025-01-01 00:32:55 +0100

    fix: use UTC for consistency

--- src/context/heatmap/heatmap.context.tsx
+++ src/context/heatmap/heatmap.context.tsx
@@ -32,7 +32,7 @@ export function HeatmapProvider({
   const [view, setView] = React.useState(IHeatmapView.HeatmapTracker);
 
   const defaultYear = useMemo(
-    () => trackerData.year ?? new Date().getFullYear(),
+    () => trackerData.year ?? new Date().getUTCFullYear(),
     [trackerData.year]
   );
 
